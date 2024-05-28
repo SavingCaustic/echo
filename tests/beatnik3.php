58ff815cdb992868bff2d4f630aa35762e7591a9
te <savingcaustic@gmail.com>
2024-05-28 21:54:46 +0200

    2track fix

--- tests/beatnik3.php
+++ tests/beatnik3.php
@@ -5,30 +5,11 @@ require('../src/core/playerEngine.php');
 $PE = new PlayerEngine();           //it doesn't have to autostart really..
 $PE->rackSetup(1,'beatnik');        //dunno really why the test-scripts would need the app? skip that.
 $myRack = $PE->getRackRef(1);
-$myBeat = $myRack->getSynthRef();
+$mySub = $myRack->getSynthRef();
 
-$PE->rackSetup(2,'subreal');        //dunno really why the test-scripts would need the app? skip that.
-$mySecondRack = $PE->getRackRef(2);
-$mySub = $mySecondRack->getSynthRef();
-
-
-//test 1 - re-trigger same note.
-$mySub->setParam('VCA_SUSTAIN', 0.4);
-$mySub->setParam('VCA_RELEASE', 200);
-$mySub->setParam('OSC2_MODLEVEL', 0.9);
-$mySub->setParam('OSC2_MODTYPE', 'FM');
-$mySub->setParam('OSC2_OCT', 0);
-$mySub->setParam('VCF_CUTOFF', 2000);
-$mySub->setParam('VCF_RESONANCE', 0.8);
-
-//$mySub->pushAllParams();
-
-$myDelay = $mySecondRack->loadEffect('delay');
-$myDelay->setParam('TIME',80/120/4);
-$myEV1 = $mySecondRack->loadEventor('octaver',1);
-
+//$myDelay = $myRack->loadEffect('delay');
 require('../utils/wavWriter.php');
-$ww = new WavWriter('2track.wav',5000);
+$ww = new WavWriter('beatnik2.wav',20000);
 $timer = microtime(true);
 
 
@@ -42,10 +23,6 @@ for($i=0;$i<16;$i++) {
     if ($i % 4 == 0) {
         $pattern[] = [$i*24, 0x90, 48, rand(90,100)];
         $pattern[] = [$i*24 + 4, 0x80, 48, 0];    
-    }
-    if ($i == 0) {
-        $pattern[] = [$i*24, 0x90, 55, 120];
-        $pattern[] = [$i*24 + 4, 0x80, 55, 0];    
     }
     if ($i == 1) {
         $pattern[] = [$i*24, 0x90, 49, rand(50,100)];
@@ -77,30 +54,11 @@ $PE->setTempo(80);
 $myRack->loadPattern($pattern, 1);
 $myRack->setSwing(48,0.5,false); //swing may also be negative!
 
-$p2 = array(
-    array(0,0x90,60,120),
-    array(0+20,0x80,60,0),
-    array(48,0x90,60,120),
-    array(48+20,0x80,60,0),
-    array(96-24,0x90,63,120),
-    array(96-24+20,0x80,63,0),
-    array(96*3,0x90,55,100),
-    array(96*3+20,0x80,55,0),
-    array(96*3+48,0x90,58,100),
-    array(96*3+48+20,0x80,58,0),
-);
-
-$mySecondRack->loadPattern($p2, 1);
-$mySecondRack->setSwing(48,0.5,false); //swing may also be negative!
-
-
 //yep, good question - where should test-render be?
 $ww->append($PE->testRender(0));
 //$app->playMode('pattern');
 $PE->play();
-for($i=0;$i<150;$i++) {
-  $ww->append($PE->testRender(2));
-}
+$ww->append($PE->testRender(200));
 $PE->stop();    //pause not working!
 $ww->append($PE->testRender(30));
 
