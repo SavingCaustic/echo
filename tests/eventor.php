cd0816a34bf87513ee162a47b568534a905f353b
SavingCaustic <savingcaustic@gmail.com>
2024-06-06 11:48:24 +0200

    eventor-test update

--- tests/eventor.php
+++ tests/eventor.php
@@ -1,8 +1,9 @@
 <?php
 //test voice allocation, polyphony and LINEAR adsr
 //note that we're generating wav from RACK, not master.
-define('SR_IF',2);                  //sample-rate inverse factor. 2 for 22050Hz, 4 for 11025.
+define('SR_IF',1);                  //sample-rate inverse factor. 2 for 22050Hz, 4 for 11025.
 
+//the timing of retrigger is incorrect, as the triggertime  gets doubled
 require('../src/core/playerEngine.php');
 $PE = new PlayerEngine();           //it doesn't have to autostart really..
 $PE->rackSetup(1,'subreal');        //dunno really why the test-scripts would need the app? skip that.
@@ -21,7 +22,7 @@ $mySub->setParam('VCA_SUSTAIN', 0.4);
 $mySub->setParam('VCA_RELEASE', 200);
 
 $myRack->parseMidi(0x90,72,120);
-$ww->append($PE->testRender(10));  
+$ww->append($PE->testRender(50));  
 $myRack->parseMidi(0x80,72,0);
 $ww->append($PE->testRender(50));  
 
